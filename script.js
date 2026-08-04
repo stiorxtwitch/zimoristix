c38740a283a450875e444ec4fac6150e3da770ca
stiorxtwitch <stiroxtwitch@gmail.com>
2026-08-04 17:25:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function () {
         clearInterval(boomInterval);
         boomInterval = setInterval(triggerBoom, 400);
         triggerBoom();
-      }, 50000));
+      }, 35000));
 
       // Arrêt automatique après 2 minutes
       feteTimeout = setTimeout(stopFete, 120000);
